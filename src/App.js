d9c93fc2e5abaef082188f363d8c65631d4e641b
Sunmoon Bappy <baappycse@gmail.com>
2019-07-16 07:22:05 +0600

    Json Data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import './Table.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+constructor(props){
+  super();
+  this.state = {
+    items:[],
+    isloaded:false,
+  }
+}
+
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(res => res.json())
+  .then(json => {
+    this.setState({
+      isloaded:true,
+      items:json,
+    })
+  });
+}
+
+  render(){
+    var { isloaded,items} = this.state;
+    if(!isloaded){
+      return <div>Loading ...</div>
+    }
+    else{
+      return (
+        <div className="App">
+          <table>
+            <thead>
+            <th>
+              <td>ID</td>
+              <td>Title</td>
+              <td>body</td>
+            </th>
+            </thead>
+            <tbody>
+              {items.map(item =>(
+                <tr>
+                  <td>{item.id}</td>
+                  <td>{item.title}</td>
+                  <td>{item.body}</td>
+                </tr>
+              ))};
+            </tbody>
+          </table>
+        </div>
+      )
+    }
+  }
 }
 
 export default App;
